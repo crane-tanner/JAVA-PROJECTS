693ecf6dd71ac375803d54706c7c98a5cf2f8e4d
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2025-03-05 21:26:17 -0700

    Finished student database app

--- StudentDatabaseApp/src/Main.java
+++ StudentDatabaseApp/src/Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //How many new students need to be added to DB?
-        String is_true = "y";
-        do {
             Scanner sc = new Scanner(System.in);
             System.out.println("Enter how many students will be added to the database?");
             int num_students = sc.nextInt();
-            if (num_students < 0 || num_students > 10) {
-                System.out.println("Number of students must be positive and less than 10.\n");
-                System.out.println("Enter how many students will be added to the database?");
-                num_students = sc.nextInt();
-            }
+            Student[] students = new Student[num_students];
 
-            Student st1 = new Student();
-            System.out.println("Do you want to continue? Type y or n");
-            is_true = sc.next();
-        } while(is_true.equals("y"));
+           for(int n = 0; n < students.length; n++) {
+               students[n] = new Student();
+               students[n].enroll();
+               students[n].payTuition();
+
+           }
+           for(int n = 0; n < num_students; n++) {
+               System.out.println(students[n].showInfo());
+           }
+
     }
 }
--- StudentDatabaseApp/src/Student.java
+++ StudentDatabaseApp/src/Student.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Student {
@@ -5,8 +6,8 @@ public class Student {
     private String lastName;
     private String gradeYear;
     private String studentID;
-    private String courses;
-    private int tuition_balance;
+    private String courses = "";
+    private int tuition_balance = 0;
     private static final int COST_PER_COURSE = 600;
     private static int id = 1000;
 
@@ -19,19 +20,49 @@ public class Student {
         lastName = in.nextLine();
         System.out.println("1 - freshman\n2 - sophomore\n3 - junior\n4 - senior\nEnter student year: ");
         gradeYear = in.nextLine();
-        System.out.println(firstName + " " + lastName + "\n" + "Grade level: " + gradeYear);
-        System.out.println("Student ID: " + setStudentID());
+        setStudentID();
 
     }
     //Generate an ID
-    private String setStudentID() {
+    private void setStudentID() {
         // Grade level + id
         id++;
         this.studentID = gradeYear + "" + id;
-        return this.studentID;
     }
     // Enroll in courses
+    public void enroll() {
+        do {
+            System.out.print("Enter a course to enroll. (Q to quit) ");
+            Scanner sc = new Scanner(System.in);
+            String course = sc.nextLine();
+            if (!Objects.equals(course, "Q")) {
+                courses = courses + "\n" +  course;
+                tuition_balance = tuition_balance + COST_PER_COURSE;
+            } else {
+                break;
+            }
+        } while(1 != 0);
+    }
     //View balance
+    public void viewBalance() {
+        System.out.println("Your balance is: $" + tuition_balance);
+    }
     // Pay tuition
+    public void payTuition() {
+        viewBalance();
+        System.out.print("Enter amount to pay: $");
+        Scanner sc = new Scanner(System.in);
+        int payment = sc.nextInt();
+        tuition_balance = tuition_balance - payment;
+        System.out.println("Thank you for your payment of $" + payment);
+        viewBalance();
+
+    }
     // show status
+    public String showInfo() {
+        return "Name: " + firstName + " " + lastName + "\n" + "Courses Enrolled: " + courses + "\n" +
+                "Grade Level: " + gradeYear + "\n" + "Student ID: " + studentID + "\nBalance: $" + tuition_balance;
+
+
+    }
 }
