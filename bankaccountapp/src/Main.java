2bc29f2b736151c2a00f2cbad16d59df894e6fbd
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2025-03-09 12:51:38 -0700

    bankingaccount application

--- bankaccountapp/src/Main.java
+++ bankaccountapp/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Checking chkacc1 = new Checking("Tom Wilson");
-        Saving savacc1 = new Saving("Michelle Johnson");
+        Checking chkacc2 = new Checking("Kai Lang");
 
       //Read a CSV file then create new accounts based on that data
 
